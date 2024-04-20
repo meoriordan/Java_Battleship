ddd2d4c168b77857ccad7036527b7dba0d9fb8d3
meoriordan <meoriordan@gmail.com>
2024-04-20 16:42:10 -0400

    starting controller files

---
+++ BattleShip/src/controller/PlayGame.java
@@ -0,0 +1,14 @@
+
+
+
+public class PlayGame {
+	
+	/*this class handles starting the game between two users who 
+	 * were connected in ConnectUsers and facilitates the game between them 
+	 * including starting the game, passing user input through board clicks back to the game 
+	 * and sending the view a message when the game is over*/
+	 */
+	
+	
+	
+}
